159f0cd397b76591924235cd7d7999fc87d2d0fc
Taufik Alex <taufikalexx13@gmail.com>
2020-09-27 21:50:53 +0700

    Initial build subtree commit

---
+++ precache-manifest.d4314ec3238d68c52b5ec1b85d6a9710.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39043acf39a2c2af5f1b070be7444dc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da19c4a55cccf6c3eae5",
+    "url": "/static/css/main.0abc29f3.chunk.css"
+  },
+  {
+    "revision": "18a737f89653ec255a02",
+    "url": "/static/js/2.e57228bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e57228bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da19c4a55cccf6c3eae5",
+    "url": "/static/js/main.5983d792.chunk.js"
+  },
+  {
+    "revision": "f438879c68bfc8864397",
+    "url": "/static/js/runtime-main.cc36b19e.js"
+  },
+  {
+    "revision": "eaa7be58abb7471cdb42bc0e20ff256f",
+    "url": "/static/media/bg.eaa7be58.jpg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "55d6093f11db95712cc18979299c6818",
+    "url": "/static/media/heroku.55d6093f.svg"
+  },
+  {
+    "revision": "dca2961968c97d2983db0c68cd062804",
+    "url": "/static/media/instagram.dca29619.svg"
+  },
+  {
+    "revision": "6c8e140e0c8b5d6acd513c67d12d267d",
+    "url": "/static/media/linkedin.6c8e140e.svg"
+  },
+  {
+    "revision": "5bb75439721af60de9f6c2902d1b94d3",
+    "url": "/static/media/state-web.5bb75439.svg"
+  }
+]);
