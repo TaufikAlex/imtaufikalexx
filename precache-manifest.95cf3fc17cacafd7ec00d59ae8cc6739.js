85e24df52456ab0ba00ea033e9ccb0e7cca2e185
Taufik Alex <taufikalexx13@gmail.com>
2020-09-27 22:16:53 +0700

    Initial build subtree commit

---
+++ precache-manifest.95cf3fc17cacafd7ec00d59ae8cc6739.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "207abe64d9ce43043f54bdf11f2c6885",
+    "url": "/taufikalex/index.html"
+  },
+  {
+    "revision": "71fe10d7378848742d9f",
+    "url": "/taufikalex/static/css/main.0abc29f3.chunk.css"
+  },
+  {
+    "revision": "18a737f89653ec255a02",
+    "url": "/taufikalex/static/js/2.e57228bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/taufikalex/static/js/2.e57228bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71fe10d7378848742d9f",
+    "url": "/taufikalex/static/js/main.09702732.chunk.js"
+  },
+  {
+    "revision": "ecc6becd83784ccb4bb9",
+    "url": "/taufikalex/static/js/runtime-main.9aed40bc.js"
+  },
+  {
+    "revision": "eaa7be58abb7471cdb42bc0e20ff256f",
+    "url": "/taufikalex/static/media/bg.eaa7be58.jpg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/taufikalex/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "55d6093f11db95712cc18979299c6818",
+    "url": "/taufikalex/static/media/heroku.55d6093f.svg"
+  },
+  {
+    "revision": "dca2961968c97d2983db0c68cd062804",
+    "url": "/taufikalex/static/media/instagram.dca29619.svg"
+  },
+  {
+    "revision": "6c8e140e0c8b5d6acd513c67d12d267d",
+    "url": "/taufikalex/static/media/linkedin.6c8e140e.svg"
+  },
+  {
+    "revision": "5bb75439721af60de9f6c2902d1b94d3",
+    "url": "/taufikalex/static/media/state-web.5bb75439.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2bddfc01ccaaf0ab9d7f711133b6b7c0.js"
+  "/taufikalex/precache-manifest.95cf3fc17cacafd7ec00d59ae8cc6739.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/taufikalex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
